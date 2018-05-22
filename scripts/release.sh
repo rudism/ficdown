b40a6abf8973d151d5010dff329e533594f4e8fc
Rudis Muiznieks <rmuiznieks@nextgxdx.com>
2018-05-22 09:47:47 -0500

    fixed publish script

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,7 +4,8 @@ DIR=`dirname $0`
 rm -rf $DIR/../*/bin $DIR/../*/obj
 xbuild /p:Configuration=Release $DIR/../Ficdown.sln
 cp -R $DIR/../Ficdown.Console/bin/Release /tmp/ficdown
-rm -f /tmp/ficdown/*.mdb
+rm -f /tmp/ficdown/*.mdb /tmp/ficdown/*.pdb
+rm /tmp/ficdown/Ficdown.Console.exe.config
 mv /tmp/ficdown/Ficdown.Console.exe /tmp/ficdown/ficdown.exe
-zip -j /tmp/ficdown.zip /tmp/ficdown/*
+7z a -tzip /tmp/ficdown.zip -w /tmp/ficdown/*
 rm -rf /tmp/ficdown
